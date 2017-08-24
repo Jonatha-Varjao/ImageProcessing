d4ccee665d6453786d4c17387df9c034136a0fb1
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-08-24 17:35:57 -0300

    TODO falta Tratar o aumento, reducao esta feita.(Vizinho)

--- Interpolation.py
+++ Interpolation.py
@@ -11,19 +11,29 @@ import sys
 #Interpolação por vizinho por 2 redução (numero de colunas par)
 def interpolacaoVizinho(img, factor):
     size = img.size
-    nwidth = int(size[0] * factor)
-    nheight = int(size[1] * factor)
+    n_X = int(size[0] * factor)
+    n_Y = int(size[1] * factor)
+    n_img = Image.new("L",(n_X, n_Y))
+    #REDUÇÃO 
+    if factor <= 1:
+        for i in range(n_X):
+            for j in range(n_Y):
+                p = (int(i/factor), int(j/factor))
+                n_img.putpixel((i,j), img.getpixel(p))
 
-    n_img = Image.new("L",(nwidth, nheight))
+        return n_img
+    #AUMENTO
+    else :
+        for i in range(n_X):
+            for j in range(n_Y):
+                p = (int(i/factor), int(j/factor))
+                n_img.putpixel((i,j), img.getpixel(p))
 
-    for i in range(nwidth):
-        for j in range(nheight):
-            p = (int(i/factor), int(j/factor))
-            n_img.putpixel((i,j), img.getpixel(p))
+        return n_img
+        print("AMPLIACAO")
 
-    return n_img
-
-
+def InterpolacaoBilinear(img, factor):
+    print("a")
 
 # MAIN passando a foto pelo CLI
 if __name__ == "__main__":
@@ -40,5 +50,4 @@ if __name__ == "__main__":
     a = interpolacaoVizinho(img, size)
     a.show()
     a.save(sys.argv[3])
-
     
