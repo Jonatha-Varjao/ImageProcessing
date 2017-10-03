dadd0ad217e785e5bb3f9124343f440a5fa9e505
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-10-03 12:34:56 -0300

    Constrat Strecth Feito
    TODO: Deixar generico

--- python/HistogramEqualization/equalization.py
+++ python/HistogramEqualization/equalization.py
@@ -2,7 +2,6 @@
 from PIL import Image
 from matplotlib import pyplot as plt
 import sys
-from itertools import izip_longest   
 
 def escalaCinza(image):
     new_image = Image.new("L",(image.size[0],image.size[1]))
@@ -23,6 +22,7 @@ def histogramGraph(image):
 def returnFrequencia(image):
     
     qtdPixels = image.size[0]*image.size[1]
+    
     list2 = image.histogram()
     PrRk = [ (float(list2[i])/float(qtdPixels),i) for i in range(256) ]    
     #print PrRk
@@ -30,11 +30,10 @@ def returnFrequencia(image):
     for i in range(256):
         #print i
         #print PrRk[i][0]
-        if i == 0:
+        if i == 0:            
             freqEq.append(255*PrRk[i][0])
-            pass
         else:
-            freqEq.append(255*PrRk[i][0] + freqEq[i-1])
+            freqEq.append( 255*(PrRk[i][0]) + freqEq[i-1] )
     #print freqEq
 
     new_image = Image.new("L",(image.size))
@@ -44,24 +43,54 @@ def returnFrequencia(image):
             m = freqEq[p]
             new_image.putpixel((i,j), int(m) )
     return new_image
-    
 
+def contrastStrech(image):
+    list = image.getcolors()
+    c =  list[0][1] 
+    d =  list[len(list) - 1][1]
+    print('{} {}'.format(c,d))
+
+    new_image = Image.new("L",(image.size))
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            p = ( image.getpixel((i,j)) - c ) * (float(255)/float((d-c)))
+            round(p)
+            if p < 0:
+                p = 0
+                
+            new_image.putpixel( (i,j), int(p) )
+            
+    return new_image
+
+    '''
+     G(F(i,j))=(F(i,j) – c) *(255/(d-c)) 
+    '''
 
 if __name__ == "__main__":
-    
-
-    if len(sys.argv) < 2 or len(sys.argv) > 2:
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
         print("compile assim 'python 'equalization.py' 'imagem' ")
         exit()
-    
-    image = Image.open(sys.argv[1])
     # RECEBER UMA IMAGEM RGB TRANSFORMA PARA CINZA 
-    if image.mode == "RGB":
-        escalaCinza(image).show()
-        print(escalaCinza(image))
-    else:
+    elif sys.argv[2] == '1' :
+        image = Image.open(sys.argv[1])
+        if image.mode == "RGB":
+            image = escalaCinza(image)
+            sys.argv[1] = "Cinza"+sys.argv[1]
+            image.save(sys.argv[1])
+            histogramGraph(image)
+            sys.argv[1]= "Equalizada"+sys.argv[1]
+            image = returnFrequencia(image)
+            image.save(sys.argv[1])
+            histogramGraph(image)
+            
+        else:
+            histogramGraph(image)
+            imageEqualizada = returnFrequencia(image)
+            sys.argv[1]= "Equalizada"+sys.argv[1]
+            imageEqualizada.save(sys.argv[1])
+            histogramGraph(imageEqualizada)
+    elif sys.argv[2] == '2':
+        image = contrastStrech(Image.open(sys.argv[1]))
+        sys.argv[1]= "ConstantStretch"+sys.argv[1]
+        image.save(sys.argv[1])
         histogramGraph(image)
-        imageEqualizada = returnFrequencia(image)
-        sys.argv[1]= "Equalizada"+sys.argv[1]
-        imageEqualizada.save(sys.argv[1])
-        histogramGraph(imageEqualizada)
