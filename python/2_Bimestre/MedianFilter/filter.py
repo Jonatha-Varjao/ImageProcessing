2d5bd9dd038fe4a8b616e8497641301cb310be5d
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-10-20 17:59:38 -0200

    melhorando o metodo da mascara 3x3

--- python/2_Bimestre/MedianFilter/filter.py
+++ python/2_Bimestre/MedianFilter/filter.py
@@ -10,23 +10,29 @@ def returnSomaVizin(image, coord ):
         return 0
     else:
         return 0
+#3x3
+def maskMedia(image, coord):
+    p1 = returnSomaVizin( image, (coord[0]-1,coord[1]-1) ) 
+    p2 = returnSomaVizin( image, (coord[0]-1,coord[1]) )
+    p3 = returnSomaVizin( image, (coord[0]-1,coord[1]+1) )
+    p4 = returnSomaVizin( image, (coord[0],coord[1]-1) )
+    p5 = returnSomaVizin( image, (coord[0],coord[1]) ) 
+    p6 = returnSomaVizin( image, (coord[0],coord[1]+1) )
+    p7 = returnSomaVizin( image, (coord[0]+1,coord[1]-1) )
+    p8 = returnSomaVizin( image, (coord[0]+1,coord[1]) )
+    p9 = returnSomaVizin( image, (coord[0]+1,coord[1]+1) )
+    pMedia = round((p1+p2+p3+p4+p5+p6+p7+p8+p9)/9)
     
+    return pMedia
+#15x15
+
+
+
 def medianFilter(image):    
     new_image = Image.new("L",(image.size),0)
     for i in range(image.size[0]):
         for j in range(image.size[1]):
-            
-            p1 = returnSomaVizin( image, (i-1,j-1) ) 
-            p2 = returnSomaVizin( image, (i-1,j) )
-            p3 = returnSomaVizin( image, (i-1,j+1) )
-            p4 = returnSomaVizin( image, (i,j-1) )
-            p5 = returnSomaVizin( image, (i,j) ) 
-            p6 = returnSomaVizin( image, (i,j+1) )
-            p7 = returnSomaVizin( image, (i+1,j-1) )
-            p8 = returnSomaVizin( image, (i+1,j) )
-            p9 = returnSomaVizin( image, (i+1,j+1) )
-            pMedia = round((p1 + (p2*2) + p3 + (p4*2) + (p5*4) + (p6*2) + p7 + (p8*2) + p9)/16)
-            
+            pMedia = maskMedia(image, (i,j))            
             new_image.putpixel((i,j), int(pMedia))
             #print("p1:{}  p2:{}  p3:{} p4:{}  p5:{}  p6:{} p7:{} p8:{}  p9:{} pMedia {} ".format(p1,p2,p3,p4,p5,p6,p7,p8,p9,pMedia))
     return new_image
