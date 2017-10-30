dc08668f2f61a908c7cadd17f81d1bb05c30e421
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-10-30 00:53:15 -0300

    Otsu Treshoulding finalizado
    Ate para imagens modo RGB

--- python/2_Bimestre/OtsuBinarization/otsu.py
+++ python/2_Bimestre/OtsuBinarization/otsu.py
@@ -1,40 +1,63 @@
-# OTSU METHOD FOR TRESHOULDING A GRAYSCALE IMAGE (256) TO A BINARY IMAGE
+# -*- coding: utf-8 -*-
+# TRESHOULDING A GRAYSCALE IMAGE AND RGB IMAGE (256) TO A BINARY IMAGE USING OTSU METHOD
 from PIL import Image
 import sys
 
-'''
-def getIndex(image, coord ):
-    try:
-        p = image.getpixel(coord)
-        return coord
-    except IndexError as e:
-        return 256
-    else:
-        return 256
-'''
+def limiarizacao(img, treshould):
+    newImage = Image.new("L", (img.size[0],img.size[1]) )
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            p = (i,j)
+            if img.getpixel(p) > treshould:
+                newImage.putpixel((i,j), 255)
+            else:
+                newImage.putpixel((i,j), 0)  
+    
+    return newImage
 
+
+def RGB_to_GrayScale(image):
+    new_image = Image.new("L",(image.size),0)
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            p = image.getpixel((i,j))
+            m = float((p[0] + p[1] + p[2])/ 3)            
+            new_image.putpixel((i,j),  int(m) )            
+    
+    return new_image
+    
 def getQtdPixelsAntes(indice, hist):
     soma = 0
     for i in range(0,indice+1):
         soma = soma + hist[i]
+    
+    if soma == 0:
+        soma = 1
+    
     return soma
 
 def getQtdPixelsDepois(indice, hist):
     soma = 0
     for i in range(indice,len(hist)):
         soma = soma + hist[i]
+    
+    if soma == 0 :
+        soma = 1
+    
     return soma
 
 def getWeightBackground(indice, hist ,qtdPixels):
     weight = 0
     for i in range(indice):
         weight = weight + hist[i]
+    
     return float(weight) / float(qtdPixels)
 
 def getWeightForeground(indice, hist ,qtdPixels):
     weight = 0
     for i in range(indice, len(hist)):
         weight = weight + hist[i]
+    
     return float(weight)/float(qtdPixels)
 
 def getMeanBackground(indice, hist):
@@ -50,6 +73,7 @@ def getMeanForeground(indice, hist):
     mean = 0
     for i in range(indice, len(hist)):
         mean =  mean + (i * hist[i])
+    
     return float(mean)/float(getQtdPixelsDepois(indice, hist))
 
 def getVarianceBackground(indice, hist, mean):
@@ -65,9 +89,6 @@ def getVarianceForeground(indice, hist, mean):
         variance =  variance + (pow((i - mean),2) * hist[i])
     
     return float(variance)/float(getQtdPixelsDepois(indice, hist))
-
-
-
 
 def otsuBinarization(image):
     hist            = image.histogram()
@@ -85,11 +106,11 @@ def otsuBinarization(image):
         Vb              =   getVarianceBackground(i, hist, Mb)
         Vf              =   getVarianceForeground(i, hist, Mf)
         Variacao.insert(i, float((Wb*Vb)+(Wf*Vf)))
-    
-    print len(Variacao)
+       
     print min(Variacao)
     print Variacao.index(min(Variacao))
     
+    return Variacao.index(min(Variacao))
 
 
 if __name__ == "__main__":
@@ -97,6 +118,17 @@ if __name__ == "__main__":
         print("compile assim 'python 'otsu.py' 'imagem' ")
         exit()
     else:
-        otsuBinarization(Image.open(sys.argv[1]))
-        #otsuBinarization(Image.open(sys.argv[1]).convert("L"))
-        print Image.open(sys.argv[1])
+        if Image.open(sys.argv[1]).mode == 'RGB':
+            #transormar em cinza 
+            imageGray =  RGB_to_GrayScale(Image.open(sys.argv[1]))
+            imageGray.show()
+            OtsuValue = otsuBinarization(imageGray)
+            limiarizacao(imageGray,OtsuValue).show()
+        elif Image.open(sys.argv[1]).mode == 'L':    
+            imageGray = Image.open(sys.argv[1])
+            imageGray.show()
+            print imageGray
+            OtsuValue = otsuBinarization(imageGray)
+            limiarizacao(imageGray, OtsuValue).show()
+        else :
+            print("Imagem não suportada")  
