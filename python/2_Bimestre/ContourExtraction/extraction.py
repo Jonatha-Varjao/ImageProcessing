9e841117d4485c8de331b146b1b5677d96a6c374
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-11-06 03:31:07 -0300

    Trocando RegionFill por Contour Extraction
    Confundi os Trabalhos

---
+++ python/2_Bimestre/ContourExtraction/extraction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("compile assim 'python 'extraction.py' 'imagem' ")
+        exit()
+    else:
+        pass
